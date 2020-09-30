a8c9cd719b297811e0e2b60b3a7c665baacad621
Arthur Gomes <ilegra.arthur@agibank.com.br>
2020-09-30 20:10:21 -0300

    adicionando classe da thaís

--- src/main/java/qp/trab/App.java
+++ src/main/java/qp/trab/App.java
@@ -1,5 +1,30 @@
 package qp.trab;
 
+import java.util.Scanner;
+
 public class App {
+
+    static final double taxaComissao = 0.15;
+
+    public static double calculaComissao(double valorVendas) {
+        return valorVendas*taxaComissao;
+    }
+
+    public static double calculaSalarioFinal(double salarioFixo, double valorVendasEfetuadas) {
+        return salarioFixo + calculaComissao(valorVendasEfetuadas);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String nome = sc.nextLine();
+        double salarioFixo = sc.nextDouble();
+        double valorVendasEfetuadas = sc.nextDouble();
+
+        double salarioComBonus = calculaSalarioFinal(salarioFixo, valorVendasEfetuadas);
+
+        System.out.printf("TOTAL = R$ %.2f", salarioComBonus);
+
+    }
 
 }
