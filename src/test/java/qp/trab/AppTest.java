682b5b825dd26d15a7a2f12432d89d422bb2cecc
Arthur-Maciel <arthur.maciel.2001@gmail.com>
2020-09-30 21:21:11 -0300

    adicionando README

--- src/test/java/qp/trab/AppTest.java
+++ src/test/java/qp/trab/AppTest.java
@@ -1,7 +1,16 @@
 package qp.trab;
 
+import org.junit.Before;
+
 import static org.junit.Assert.*;
 
 public class AppTest {
+
+    private App app;
+
+    @Before
+    public void intialize(){
+
+    }
 
 }
